8e3f4e8caf1a0df7c2efca5a4a6caf31bd41b8f2
NikhilPrakash01 <nikhilnawaab303@gmail.com>
2024-10-04 15:08:53 +0530

    first commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+const About = () => {
+  return (
+    <div>
+      <div className="text-center text-2xl pt-10 text-gray-500">
+        <p>
+          ABOUT <span className="text-green-400 font-medium">US</span>
+        </p>
+      </div>
+
+      <div className="my-10 flex flex-col md:flex-row gap-12">
+        <img
+          className="w-full md:max-w-[360px]"
+          src={assets.about_image}
+          alt=""
+        />
+        <div className="flex flex-col justify-center gap-6 md:w-2/4 text-sm text-gray-600">
+          <p>
+            Welcome to Prescripto At Prescripto, we aim to revolutionize the way
+            you manage your healthcare. We make booking doctor appointments
+            seamless, efficient, and hassle-free. Whether it's scheduling
+            routine check-ups or finding the right specialist, we’re here to
+            ensure that your health is always a priority.
+          </p>
+          <p>
+            Committed to Your Health We are committed to connecting you with
+            experienced doctors from various fields, ensuring timely care with
+            the best medical advice. Our easy-to-use platform is designed to
+            save you time and provide you with reliable healthcare options
+            whenever you need them.
+          </p>
+          <b className="text-gray-800">Our Vision</b>
+          <p>
+            Our vision is to create a healthcare ecosystem where everyone has
+            access to quality medical care with just a few clicks. We strive to
+            simplify the doctor-patient interaction, making healthcare more
+            accessible and convenient for people from all walks of life.
+          </p>
+        </div>
+      </div>
+      <div className="text-xl my-4">
+        <p>
+          WHY <span className="text-gray-700 font-semibold">CHOOSE US</span>{" "}
+        </p>
+      </div>
+      <div className="flex flex-col md:flex-row mb-20">
+        <div className="border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer">
+          <b>Efficiency:</b>
+          <p>
+            Streamlined appointment scheduling that fits into your busy
+            lifestyle.
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer">
+          <b>Convenience:</b>
+          <p>
+            Access to a network of trusted healthcare professionals in your
+            area.
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer">
+          <b>Personalization:</b>
+          <p>
+            Tailored recommendations and reminders to help you stay on top of
+            your health.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
